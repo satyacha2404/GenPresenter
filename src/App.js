6427cbcdd1409539b3327e302b011b22b2ed272c
satyacha2404 <119803650+satyacha2404@users.noreply.github.com>
2023-05-19 23:00:24 +0700

    Modify UI

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { useRef } from 'react';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
@@ -24,54 +25,52 @@ const Item = styled(Paper)(({ theme }) => ({
 const src = "";
 
 function App() {
+  const windowSize = useRef([window.innerWidth, window.innerHeight]);
+
   return (
     <div> 
-      <Box sx={{ flexGrow: 1 }}>
+      <Box sx={{ flexGrow: 11 }}>
         <AppBar position="static">
-          <Toolbar>
+          <Toolbar style={{backgroundColor: "#00a758", fontSize: 40, height: 100, fontWeight: 'bold'}}>
             Go Presenter
           </Toolbar>
         </AppBar>
-        <Card>
+        <Card style={{backgroundColor: "#00a758", height: windowSize.current[1] - 100}}>
           <CardContent>
             <Box sx={{ m: 10 }} /> 
             <Box sx={{ flexGrow: 1 }}>
               <Grid container spacing={10}>
-                <Grid item xs={1} md={1}>
+                <Grid item xs={2} md={2}>
                 </Grid>
                 <Grid item xs={3} md={3}>
-                  <Item>
+                  <Item style={{padding: 50, borderRadius: 50}}>
                     <TextField fullWidth label="Nama" variant="outlined" width="100%" helperText=" "/>
                     <TextField fullWidth label="Jenis Kelamin" variant="outlined" helperText=" "/>
                     <TextField fullWidth label="Tanggal Lahir" variant="outlined" helperText=" "/>
                     <TextField fullWidth label="Domisili" variant="outlined" helperText=" "
                               multiline
-                              rows={4}/>
-                  </Item>
-                </Grid>
-                <Grid item xs={1} md={1}>
-                </Grid>
-                <Grid item xs={6} md={6}>
-                  <Item>
-                    <Stack direction="row" spacing={2}>
+                              rows={5}/>
+                    <Stack spacing={2}>
                       <Button variant="contained" endIcon={<CameraAlt />}>
                         Take Photo
                       </Button>
                     </Stack>                    
                   </Item>
-                  <Item>
-                    <video controls width="100%" autoPlay={true}>
+                </Grid>
+                <Grid item xs={6} md={6}>
+                  <Item style={{padding: 50, borderRadius: 50}}>
+                    <Stack direction="column" spacing={2}>
+                    <video controls autoPlay={true}>
                       <source src={src} type="video/mp4" />
                         Sorry, your browser doesn't support embedded videos.
                       </video>    
-                  </Item>
-                  <Item>
-                    <Stack direction="row" spacing={2}>
                       <Button variant="contained" endIcon={<VideoLibraryIcon />}>
                         Generate Video
                       </Button>
                     </Stack>                    
                   </Item>
+                </Grid>
+                <Grid item xs={1} md={1}>
                 </Grid>
               </Grid>
             </Box>         
